b7f3d289e41b77c5a5245c7f4fb3bb800111d8f4
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-05-29 00:00:14 +0400

    05/29/2024

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MyPlaceController; //добавить полный путь к контроллеру
+use App\Http\Controllers\PostController; //добавить полный путь к контроллеру
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [MyPlaceController::class, 'index']); //задать контроллер как класс, с указанием метода
+Route::get('/posts', [PostController::class, 'index']); //задать контроллер как класс, с указанием метода
