a2f3c7e03e92c99e1fae4b8be0a88163c1c8e23d
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-05-31 00:02:03 +0400

    55

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index() {
-        return 'this is my page';
+        $posts = Post::all();
+        dd($posts);
     }
 }
